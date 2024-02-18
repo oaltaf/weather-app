88128694c5082b53a34d6fd72d4a9baa3f331a25
Osama <osama.altaf385@gmail.com>
2024-02-19 00:29:43 +0500

    changes in page file for weather api connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,63 @@
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
+interface Coord {
+  lon: number;
+  lat: number;
+}
+
+interface Weather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+interface Main {
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  pressure: number;
+  humidity: number;
+}
+
+interface Wind {
+  speed: number;
+  deg: number;
+}
+
+interface Clouds {
+  all: number;
+}
+
+interface Sys {
+  type: number;
+  id: number;
+  country: string;
+  sunrise: number;
+  sunset: number;
+}
+
+interface WeatherData {
+  coord: Coord;
+  weather: Weather[];
+  base: string;
+  main: Main;
+  visibility: number;
+  wind: Wind;
+  clouds: Clouds;
+  dt: number;
+  sys: Sys;
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+}
+
+//https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+//https://api.openweathermap.org/data/2.5/weather?q=Lahore&appid=48a07e6a1a6d1180fbac61a592b85def
+
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,5 +1,7 @@
+import { cn } from '@/utils/cn';
 import React from 'react'
 import { IoSearch } from "react-icons/io5";
+
 
 type Props = {
     className?: string;
